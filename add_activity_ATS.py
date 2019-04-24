27adeedc10aeaae54214646eb6397185c8fd6014
amizar1 <amizar@unomaha.edu>
2019-04-23 22:59:52 -0500

    created and updated by Amber

--- add_activity_ATS.py
+++ add_activity_ATS.py
@@ -10,27 +10,27 @@ class AddActivity_ATS(unittest.TestCase):
        self.driver = webdriver.Chrome()
 
    def test_add_activity(self):
-       user = "instructor"
+       user = "instructor1"
        pwd = "instructor1a"
 
        driver = self.driver
        driver.maximize_window()
-       driver.get("https://blai.pythonanywhere.com/login")
+       driver.get("https://cvong1001.pythonanywhere.com/login")
        elem = driver.find_element_by_id("id_username")
        elem.send_keys(user)
        elem = driver.find_element_by_id("id_password")
        elem.send_keys(pwd)
        elem.send_keys(Keys.RETURN)
-       driver.get("https://blai.pythonanywhere.com")
+       driver.get("https://cvong1001.pythonanywhere.com")
        assert "Logged In"
        time.sleep(2)
 
        #path to view activity list
-       driver.get("https://blai.pythonanywhere.com/activity_list")
+       driver.get("https://cvong1001.pythonanywhere.com/activity_list")
        time.sleep(2)
 
        # path for Add activity
-       elem = driver.find_element_by_xpath("html/body/div/div/div/div[3]/div/a/span").click()
+       elem = driver.find_element_by_xpath("/html/body/div/div/div/div[3]/div/a/span").click()
        time.sleep(2)
 
        elem = driver.find_element_by_id("id_title")
@@ -64,7 +64,7 @@ class AddActivity_ATS(unittest.TestCase):
        time.sleep(2)
        assert "New activity Created"
 
-       driver.get("https://blai.pythonanywhere.com/activity_list")
+       driver.get("https://cvong1001.pythonanywhere.com/activity_list")
        time.sleep(3)
 
        def tearDown(self):
--- edit_activity_ATS.py
+++ edit_activity_ATS.py
@@ -10,23 +10,23 @@ class AddActivity_ATS(unittest.TestCase):
        self.driver = webdriver.Chrome()
 
    def test_add_activity(self):
-       user = "instructor"
+       user = "instructor1"
        pwd = "instructor1a"
 
        driver = self.driver
        driver.maximize_window()
-       driver.get("https://blai.pythonanywhere.com/login")
+       driver.get("https://cvong1001.pythonanywhere.com/login")
        elem = driver.find_element_by_id("id_username")
        elem.send_keys(user)
        elem = driver.find_element_by_id("id_password")
        elem.send_keys(pwd)
        elem.send_keys(Keys.RETURN)
-       driver.get("https://blai.pythonanywhere.com")
+       driver.get("https://cvong1001.pythonanywhere.com")
        assert "Logged In"
        time.sleep(2)
 
        #path to view activity list
-       driver.get("https://blai.pythonanywhere.com/activity_list")
+       driver.get("https://cvong1001.pythonanywhere.com/activity_list")
        time.sleep(2)
 
        # path for Add activity
@@ -49,7 +49,7 @@ class AddActivity_ATS(unittest.TestCase):
        time.sleep(2)
        assert "Activity Updated"
 
-       driver.get("https://blai.pythonanywhere.com/activity_list")
+       driver.get("https://cvong1001.pythonanywhere.com/activity_list")
        time.sleep(3)
 
        def tearDown(self):
